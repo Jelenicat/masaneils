46e8fdf8af2119bfe249efaf6cf9820dff8bd556
Jelena Tanasković <jelenatanaskovicj@gmail.com>
2025-07-13 19:11:05 +0200

    kk

--- src/components/VerticalScheduleView.js
+++ src/components/VerticalScheduleView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { format, startOfWeek, addDays, differenceInMinutes } from "date-fns";
 import { db } from "../firebase";
 import {

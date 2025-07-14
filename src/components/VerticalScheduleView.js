36815f0add1985ea001c42d5cd8df09c4d7aa4ec
Jelena Tanasković <jelenatanaskovicj@gmail.com>
2025-07-14 09:54:58 +0200

    k

--- src/components/VerticalScheduleView.js
+++ src/components/VerticalScheduleView.js
@@ -43,19 +43,19 @@ const VerticalScheduleView = ({
               <h4>
                 {dan} {format(currentDate, "dd. MMM", { locale: srLatn })}
               </h4>
-              <button
-                className="add-button"
-                onClick={() => {
-               const start = new Date(currentDate);
-start.setHours(9, 0, 0, 0);
-const end = new Date(currentDate);
-end.setHours(10, 0, 0, 0);
+       <button
+  className="add-button"
+  onClick={() => {
+    const start = new Date(currentDate);
+    start.setHours(9, 0, 0, 0);
+    const end = new Date(currentDate);
+    end.setHours(10, 0, 0, 0);
+    onSelectSlot({ start, end, tip: "slobodan" });
+  }}
+>
+  Dodaj termin
+</button>
 
-                  onSelectSlot({ start, end });
-                }}
-              >
-                Dodaj termin
-              </button>
             </div>
 
             {dailyEvents.length === 0 ? (
